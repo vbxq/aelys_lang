dfbd66fc133716b829e8878fa7a5f6a79f8aae4f
vbxq <256484177+vbxq@users.noreply.github.com>
2026-02-24 10:53:44 +0100

    chore: small buildscript

---
+++ _build.ps1
@@ -0,0 +1,11 @@
+$vs = & "${env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere.exe" -latest -products * -requires Microsoft.VisualStudio.Component.VC.Tools.x86.x64 -property installationPath
+$vc = "$vs\VC\Auxiliary\Build\vcvars64.bat"
+cmd /c ('"' + $vc + '" >nul 2>&1 && set') | ForEach-Object {
+    if ($_ -match '^(.*?)=(.*)$') {
+        Set-Item -Path "Env:$($matches[1])" -Value $matches[2]
+    }
+}
+$env:LLVM_SYS_181_PREFIX = 'C:\llvm'
+$env:PATH = "C:\llvm\bin;$env:PATH"
+Set-Location 'C:\Users\admin\RustroverProjects\aelys_lang'
+cargo test
