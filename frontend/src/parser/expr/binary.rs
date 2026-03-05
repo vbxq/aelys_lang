587171008be3406a7afba77eea8c5f2c0ecdac30
vbxq <256484177+vbxq@users.noreply.github.com>
2026-03-05 18:25:32 +0100

    NFC: reduce duplication in binary.rs

--- frontend/src/parser/expr/binary.rs
+++ frontend/src/parser/expr/binary.rs
@@ -6,150 +6,57 @@ use aelys_syntax::{Expr, ExprKind, TokenKind};
 
 impl Parser {
     pub(super) fn bit_or(&mut self) -> Result<Expr> {
-        let mut left = self.bit_xor()?;
-
-        while let Some(op) = self.match_binary_op(&[TokenKind::Pipe]) {
-            let right = self.bit_xor()?;
-            let span = left.span.merge(right.span);
-            left = Expr::new(
-                ExprKind::Binary {
-                    left: Box::new(left),
-                    op,
-                    right: Box::new(right),
-                },
-                span,
-            );
-        }
-
-        Ok(left)
+        self.parse_left_assoc(Self::bit_xor, &[TokenKind::Pipe])
     }
 
     fn bit_xor(&mut self) -> Result<Expr> {
-        let mut left = self.bit_and()?;
-
-        while let Some(op) = self.match_binary_op(&[TokenKind::Caret]) {
-            let right = self.bit_and()?;
-            let span = left.span.merge(right.span);
-            left = Expr::new(
-                ExprKind::Binary {
-                    left: Box::new(left),
-                    op,
-                    right: Box::new(right),
-                },
-                span,
-            );
-        }
-
-        Ok(left)
+        self.parse_left_assoc(Self::bit_and, &[TokenKind::Caret])
     }
 
     fn bit_and(&mut self) -> Result<Expr> {
-        let mut left = self.equality()?;
-
-        while let Some(op) = self.match_binary_op(&[TokenKind::Ampersand]) {
-            let right = self.equality()?;
-            let span = left.span.merge(right.span);
-            left = Expr::new(
-                ExprKind::Binary {
-                    left: Box::new(left),
-                    op,
-                    right: Box::new(right),
-                },
-                span,
-            );
-        }
-
-        Ok(left)
+        self.parse_left_assoc(Self::equality, &[TokenKind::Ampersand])
     }
 
     fn equality(&mut self) -> Result<Expr> {
-        let mut left = self.comparison()?;
-
-        while let Some(op) = self.match_binary_op(&[TokenKind::EqEq, TokenKind::BangEq]) {
-            let right = self.comparison()?;
-            let span = left.span.merge(right.span);
-            left = Expr::new(
-                ExprKind::Binary {
-                    left: Box::new(left),
-                    op,
-                    right: Box::new(right),
-                },
-                span,
-            );
-        }
-
-        Ok(left)
+        self.parse_left_assoc(Self::comparison, &[TokenKind::EqEq, TokenKind::BangEq])
     }
 
     fn comparison(&mut self) -> Result<Expr> {
-        let mut left = self.shift()?;
-
-        while let Some(op) = self.match_binary_op(&[
-            TokenKind::Lt,
-            TokenKind::LtEq,
-            TokenKind::Gt,
-            TokenKind::GtEq,
-        ]) {
-            let right = self.shift()?;
-            let span = left.span.merge(right.span);
-            left = Expr::new(
-                ExprKind::Binary {
-                    left: Box::new(left),
-                    op,
-                    right: Box::new(right),
-                },
-                span,
-            );
-        }
-
-        Ok(left)
+        self.parse_left_assoc(
+            Self::shift,
+            &[
+                TokenKind::Lt,
+                TokenKind::LtEq,
+                TokenKind::Gt,
+                TokenKind::GtEq,
+            ],
+        )
     }
 
     fn shift(&mut self) -> Result<Expr> {
-        let mut left = self.term()?;
-
-        while let Some(op) = self.match_binary_op(&[TokenKind::Shl, TokenKind::Shr]) {
-            let right = self.term()?;
-            let span = left.span.merge(right.span);
-            left = Expr::new(
-                ExprKind::Binary {
-                    left: Box::new(left),
-                    op,
-                    right: Box::new(right),
-                },
-                span,
-            );
-        }
-
-        Ok(left)
+        self.parse_left_assoc(Self::term, &[TokenKind::Shl, TokenKind::Shr])
     }
 
     fn term(&mut self) -> Result<Expr> {
-        let mut left = self.factor()?;
-
-        while let Some(op) = self.match_binary_op(&[TokenKind::Plus, TokenKind::Minus]) {
-            let right = self.factor()?;
-            let span = left.span.merge(right.span);
-            left = Expr::new(
-                ExprKind::Binary {
-                    left: Box::new(left),
-                    op,
-                    right: Box::new(right),
-                },
-                span,
-            );
-        }
-
-        Ok(left)
+        self.parse_left_assoc(Self::factor, &[TokenKind::Plus, TokenKind::Minus])
     }
 
     fn factor(&mut self) -> Result<Expr> {
-        let mut left = self.unary()?;
+        self.parse_left_assoc(
+            Self::unary,
+            &[TokenKind::Star, TokenKind::Slash, TokenKind::Percent],
+        )
+    }
 
-        while let Some(op) =
-            self.match_binary_op(&[TokenKind::Star, TokenKind::Slash, TokenKind::Percent])
-        {
-            let right = self.unary()?;
+    fn parse_left_assoc(
+        &mut self,
+        lower_precedence: fn(&mut Self) -> Result<Expr>,
+        operators: &[TokenKind],
+    ) -> Result<Expr> {
+        let mut left = lower_precedence(self)?;
+
+        while let Some(op) = self.match_binary_op(operators) {
+            let right = lower_precedence(self)?;
             let span = left.span.merge(right.span);
             left = Expr::new(
                 ExprKind::Binary {
